97ab2f0cc8a1becde35adbf47c3d29d27e6e0550
Ronald Magno <dlanoronald123@gmail.com>
2021-09-29 22:40:51 +0800

    Added function to modal

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from '../UI/Card'
-import classes from './UsersList.module.css'
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
   return (
